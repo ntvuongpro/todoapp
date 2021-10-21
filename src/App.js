cd57e59d98f6e6b891057a05b9cb16c72c176171
ntvuongpro <hawkiee.ntv@gmail.com>
2021-10-21 21:00:31 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoList from './components/TodoList';
+//import './css/TodoCSS.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+
+    </div>
+  );
+}
+
+export default App;
